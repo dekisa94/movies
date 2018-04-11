7c64a4340a5d53dcd04608d314fe0b53f85d70df
dekisa94 <dsimic94@gmail.com>
2018-04-11 20:06:47 +0200

    Task4 i Task5

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -7,11 +7,28 @@
   <title>@yield('title')</title>
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta name="author" content="">
-  <link rel="stylesheet" href="css/style.css">
-  <script src="http://code.jquery.com/jquery-latest.min.js"></script>
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+  <link href="css/app.css" rel="stylesheet">
 </head>
 
 <body>
+<nav class="navbar navbar-expand-md navbar-dark fixed-top bg-dark">
+      <a class="navbar-brand" href="/movies">Movies</a>
+      <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarCollapse" aria-controls="navbarCollapse" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarCollapse">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item active">
+            <a class="nav-link" href="/movies">Home <span class="sr-only">(current)</span></a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="/movies/add">Add movie</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
   
 <div class="container">
     @yield('content')
--- resources/views/show.blade.php
+++ resources/views/show.blade.php
@@ -5,6 +5,7 @@ Movies
 @endsection
 
 @section('content')
+    <br><br><br>
     {{$movie->title}}
     <hr>
     {{$movie->storyline}}
