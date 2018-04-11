475faf3d6688bd43cb7c7b5aa1cd960e0e0d3355
dekisa94 <dsimic94@gmail.com>
2018-04-11 19:12:19 +0200

    Task2

---
+++ database/migrations/2018_04_11_165848_create_movies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('ganre');
+            $table->string('director');
+            $table->integer('year');
+            $table->string('storyline');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
+
+//Polja u tabeli: id, title, žanr (genre), ime režisera (director), godinu kada je snimljen i storyline (zaplet)
+
